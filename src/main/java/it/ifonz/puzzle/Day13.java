b6878ab66e0425b8f86be74624801bf0556eddcd
Gianluca di Fonzo <gdifonzo89@gmail.com>
2017-12-13 14:37:52 +0100

    d13 p2
    added + refactored full d13 solution

--- src/main/java/it/ifonz/puzzle/Day13.java
+++ src/main/java/it/ifonz/puzzle/Day13.java
@@ -5,7 +5,6 @@ import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 
 import it.ifonz.common.FileReader;
 
@@ -22,32 +21,39 @@ public class Day13 {
 
 	public static long part1(List<String> input) {
 
+		HashMap<Integer, Integer> firewall = createFirewall(input);
+
+		// each layer needs 2*(range-1) steps for a round-trip scan. the packet
+		// reaches layer N after N steps
+		return firewall.entrySet().stream()
+				.filter(e -> e.getKey().intValue() % (2 * (e.getValue().intValue() - 1)) == 0)
+				.mapToLong(e -> e.getKey() * e.getValue()).sum();
+
+	}
+
+	public static long part2(List<String> input) {
+		HashMap<Integer, Integer> firewall = createFirewall(input);
+		AtomicInteger delay = new AtomicInteger(0);
+		// same logic, but the packet reaches layer N after N+delay steps
+		// if at least ONE layer catches the packet, then increment the delay
+		// and retest again
+		while (firewall.entrySet().stream()
+				.anyMatch(e -> (delay.get() + e.getKey().intValue()) % (2 * (e.getValue().intValue() - 1)) == 0)) {
+			delay.incrementAndGet();
+		}
+		return delay.get();
+
+	}
+
+	// parsing the input
+	private static HashMap<Integer, Integer> createFirewall(List<String> input) {
 		HashMap<Integer, Integer> firewall = new HashMap<>();
-		AtomicInteger max = new AtomicInteger();
 		input.forEach(i -> {
 			String[] split = i.split(":");
 			Integer valueOf = Integer.valueOf(split[0]);
 			firewall.put(valueOf, Integer.valueOf(split[1]));
-			max.set(valueOf);
 		});
-
-		AtomicInteger sum = new AtomicInteger(0);
-		IntStream.rangeClosed(0, max.get()).forEach(i -> {
-			Integer integer = firewall.get(i);
-			if (integer != null)
-				if (i % (2 * (integer-1)) == 0) {
-					sum.addAndGet(i * integer);
-				}
-		});
-
-		return sum.get();
-
-	}
-
-	public static boolean part2(List<String> input) {
-
-		return false;
-
+		return firewall;
 	}
 
 }
